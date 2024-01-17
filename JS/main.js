142614c1d9a6712366e237da853ef1adc91f1f03
Soumya Chakrabortty <soumyalal1991@live.com>
2024-01-17 22:23:06 +0530

    First Commit

---
+++ JS/main.js
@@ -0,0 +1,4 @@
+import darkMode from './dark-mode.js';
+
+
+darkMode();
---
+++ JS/sidemenu.js
@@ -0,0 +1,12 @@
+const sidemenu  = document.getElementById("sidemenu");
+// const menubar = document.getElementById("menubar");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+    // menubar.style.display = "none";
+}
+
+function closemenu(){
+    sidemenu.style.right = "-20rem";
+    // menubar.style.display = "block";
+}
